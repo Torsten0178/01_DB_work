194aad561b25f2a0bce65b0a19806cf69f65e915
“Torsten” <at.torsten@gmail.com>
2025-02-28 12:48:13 +0100

    blubb

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -1,5 +1,5 @@
+\! cls
 /* ------  Strukturen ----- */
-
 /* Kommentar 1 */
 #  Kommentar 2
 -- Kommentar 3
@@ -8,23 +8,31 @@
 SHOW DATABASES;
 
 /* DB boo löschen, falls vorhanden*/
-DROP DATABASE boo;
+DROP DATABASE IF EXISTS boo ;
 
 /* DB boo anlegen, falls noch nicht vorhanden*/
-
+CREATE DATABASE if NOT EXISTS boo;
 
 /* DB auswählen */
+USE boo;
 
 /* Tabelle anlegen */
-
+CREATE TABLE If NOT EXISTS test
+(
+    name VARCHAR(20),
+    age INT
+);
 
 /* Alle Tabellen in der DB anzeigen */
-
+SHOW TABLES;
+-- SHOW TABLES FROM mysql;
 
 /* Struktur der Tabelle anzeigen */
-
+DESCRIBE test;
 
 /* ----- Daten ------- */
-
-
+INSERT INTO test(name,age) VALUES("Max",35);
+INSERT INTO test(age,name) VALUES(29,"Maxine");
+INSERT INTO test VALUES();
 /* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM test;

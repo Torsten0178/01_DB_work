41aac8eafa049285a24574b5f45b582e42af8ba4
“Torsten” <at.torsten@gmail.com>
2025-03-03 12:04:41 +0100

    blubb

--- 02_SQL_Tables/02d_db_table_SQL.sql
+++ 02_SQL_Tables/02d_db_table_SQL.sql
@@ -1,38 +1,37 @@
 \! cls
 /* ------  Strukturen ----- */
-/* Kommentar 1 */
-#  Kommentar 2
--- Kommentar 3
 
-/* Datenbanken auf Server anzeigen */
-SHOW DATABASES;
+/* 	 
+	NULL wird nicht mehr zugelassen
+    DEFAULT-Werte eintragen
+*/
 
-/* DB boo löschen, falls vorhanden*/
-DROP DATABASE IF EXISTS boo ;
 
-/* DB boo anlegen, falls noch nicht vorhanden*/
-CREATE DATABASE if NOT EXISTS boo;
 
-/* DB auswählen */
-USE boo;
+/* Tabelle test löschen, falls vorhanden*/
+DROP TABLE IF EXISTS test;
 
-/* Tabelle anlegen */
-CREATE TABLE If NOT EXISTS test
+/* Tab. test anlegen, falls noch nicht vorhanden*/
+CREATE TABLE IF NOT EXISTS test
 (
-    name VARCHAR(20),
-    age INT
+	name VARCHAR(20) NOT NULL DEFAULT "TBA",
+	age INT NOT NULL DEFAULT 0
 );
 
 /* Alle Tabellen in der DB anzeigen */
 SHOW TABLES;
--- SHOW TABLES FROM mysql;
 
 /* Struktur der Tabelle anzeigen */
 DESCRIBE test;
 
 /* ----- Daten ------- */
-INSERT INTO test(name,age) VALUES("Max",35);
-INSERT INTO test(age,name) VALUES(29,"Maxine");
-INSERT INTO test VALUES();
+INSERT INTO test(name,age) VALUES ("Maxine",29);
+INSERT INTO test(age,name) VALUES (35,"Max");
+INSERT INTO test VALUES ();
+
+-- ABER: Doppelte Datensätze werden zugelassen !
+INSERT INTO test(age,name) VALUES (35,"Max");
+
 /* ---- Inhalte der Tabelle anzeigen ---- */
 SELECT * FROM test;
+
